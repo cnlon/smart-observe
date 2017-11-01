d3242c1983081a1a2c126e792feb257608306273
lon <longhaohe@gmail.com>
2017-11-01 20:34:43 +0800

    Update dependencies and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,46 @@
 import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
 
 const needMin = !!process.env.MIN
-let dest = 'dist/smart-observe.js'
+let file = 'dist/smart-observe.js'
 const plugins = [
   resolve({
     jsnext: true,
     browser: true,
   }),
-  commonjs(),
   babel({
-    presets: ['es2015-rollup'],
-    plugins: ['transform-class-properties'],
-  }),
+    presets: [
+      ['env', {
+        targets: {
+          browsers: ['last 3 versions', 'IE >= 9'],
+          node: '4'
+        },
+        modules: false
+      }]
+    ],
+    plugins: [
+      'transform-class-properties',
+      'external-helpers'
+    ],
+  })
 ]
 if (needMin) {
-  dest = 'dist/smart-observe.min.js'
+  file = 'dist/smart-observe.min.js'
   plugins.push(uglify())
 }
 
 export default {
-  entry: 'src/index.js',
-  format: 'umd',
-  moduleId: 'observe',
-  moduleName: 'observe',
-  dest,
+  input: 'src/index.js',
+  output: {
+    file,
+    format: 'umd',
+    name: 'observe',
+    banner:
+    `/**
+     * smart-observe --- By lon
+     * https://github.com/cnlon/smart-observe
+     */`,
+  },
   plugins,
-  banner:
-`/**
- * smart-observe --- By lon
- * https://github.com/cnlon/smart-observe
- */`,
 }
